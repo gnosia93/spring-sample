074a38ce004a710740da6d385c48b85787f91216
home <gnosia93@naver.com>
2018-12-11 01:53:18 +0900

    mock mvc.. junit

--- src/main/java/com/sbk/springsample/order/controller/OrderController.java
+++ src/main/java/com/sbk/springsample/order/controller/OrderController.java
@@ -4,30 +4,36 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sbk.springsample.order.command.AddOrderCommand;
-import com.sbk.springsample.order.domain.Buyer;
 
 @RestController
 @RequestMapping(value="/order")
 public class OrderController {
 
 	Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	
+	private ResponseEntity<?> jsonResponse(String body) {
+
+		return ResponseEntity.ok()
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.body(body);
+
+	}
+	
 	
 	@RequestMapping(value="/add", method=RequestMethod.POST, consumes="application/json", produces="application/json")
-	public ResponseEntity<?> addOrder(AddOrderCommand addOrderCommand) {
+	public ResponseEntity<?> addOrder(@RequestBody AddOrderCommand addOrderCommand) {
 	
 		System.out.println(addOrderCommand);
+		System.out.println(".....xxxsx");
 		
-		
-		
-		return ResponseEntity
-			.ok()
-			.contentType(MediaType.APPLICATION_JSON_UTF8)
-			.body("...");
+		return jsonResponse("......");
 	}
 	
 	
--- src/test/java/com/sbk/springsample/OrderControllerTest.java
+++ src/test/java/com/sbk/springsample/OrderControllerTest.java
@@ -1,36 +1,63 @@
 package com.sbk.springsample;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import com.sbk.springsample.order.command.AddOrderCommand;
+import com.sbk.springsample.order.domain.Item;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
+@WebMvcTest
 public class OrderControllerTest {
 
-	MockMvc mockMvc;
-	ObjectMapper objectMapper = new ObjectMapper();
+	@Autowired MockMvc mockMvc;
+	@Autowired ObjectMapper objectMapper; 
+	
+	private AddOrderCommand makeAddOrderCommand() {
+		List<Item> itemList = new ArrayList<>();
+		Item item = new Item();
+		item.setItemId(1);
+		item.setItemName("item name-1");
+		item.setItemCount(1);
+		itemList.add(item);
+		
+		AddOrderCommand addOrderCommand = new AddOrderCommand();
+		addOrderCommand.setOrderNo(0);
+		addOrderCommand.setTimestamp(Instant.now());
+		addOrderCommand.setItemList(itemList);
+		
+		return addOrderCommand;
+	}
+	
 	
 	
 	@Test
 	public void testCase() throws Exception {
-		String content = "";
+		AddOrderCommand addOrderCommand = makeAddOrderCommand();
+		String contentBody = objectMapper.writeValueAsString(addOrderCommand);
 		
 		mockMvc.perform(post("/order/add")
 				.contentType(MediaType.APPLICATION_JSON_UTF8)
-				.content(content))
-			.andExpect(status().isOk())
-			.andDo(print());
+				.content(contentBody))
+			.andExpect(status().isOk());
+			// .andExpect(Response<T>)
+			//.andDo(print());
+			// response check 하는 코드 추가. 
 	}
 	
 	
