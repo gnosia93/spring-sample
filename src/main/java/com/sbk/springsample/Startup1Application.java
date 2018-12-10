c04da1a6deed10933dfececc885431ffe267f5be
home <gnosia93@naver.com>
2018-12-10 22:24:05 +0900

    init comit

---
+++ src/main/java/com/sbk/springsample/Startup1Application.java
@@ -0,0 +1,12 @@
+package com.sbk.springsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup1Application.class, args);
+	}
+}
